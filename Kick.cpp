31fcf7ec3170f4e61393319fdfe1232980c96ffb
speedstarskiwi <109442847+Speedstarskiwi@users.noreply.github.com>
2022-08-03 15:51:18 -0400

    Update Kick.cpp

--- Kick.cpp
+++ Kick.cpp
@@ -14,3 +14,20 @@ void KickPlayer(DWORD rL, const char* reason)
 /* aob -> 55 8B EC 51 8B 55 08 8B C2 56 8B F1 57 8D 78 01 */
 
 KickPlayer(rL, "You have been kicked because you were a skid");
+
+DWORD KickPlayer2(DWORD a, int b, unsigned int a3)
+{
+	bool result = 0x0000000; /* null */
+	DWORD Kick_Addy = (0x689100 - 0x400000 + (DWORD)GetModuleHandleA(NULL /* replace first dword with new address */
+
+	using Kick_Def = DWORD(__thiscall*)(DWORD a1, int b2, unsigned int 33); /* arguments with convention */
+	Kick_Def Kick = reinterpret_cast<Kick_Def>(Kick_Addy);
+									 
+        Kick(a, b, a3);
+        return result;
+}
+									 
+KickPlayer2(rL, (int)"You were kicked from this game: No reason we just felt like kicking you lmfao!", (unsigned int)0x32u);
+/* 8B F1 57 8B 7D 0C 8B 4E 14 */
+									 
+/* this is the second kick code, it is a backup and reviver to the first one */
